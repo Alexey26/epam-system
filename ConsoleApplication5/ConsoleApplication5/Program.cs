b2a15b238fb2fef0321170ed2de5317cb6c149b6
Alexey <fomenkolexa@yandex.ru>
2018-02-02 17:12:00 +0400

    fifth

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int [] { 2, 5, 3, 4, 0, 5, 2, 0, 4, 3, 100 };
+            int[] kol = new int[255];
+            for (int i=0; i < mass.Length; i++)
+                kol[mass[i]]++;
+            for (int i = 0; i<255 ; i++)
+            {
+                if (kol[i] == 1)
+                    Console.WriteLine (i);
+            }
+            Console.ReadKey ();
+        }
+    }
+}

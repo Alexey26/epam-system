3cef0ce570f72cb002a242a0dcf5bbf037f8c016
Alexey <fomenkolexa@yandex.ru>
2018-02-02 17:27:29 +0400

    sixth

--- ConsoleApplication5/ConsoleApplication5/Program.cs
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -10,16 +10,22 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            int[] mass = new int [] { 2, 5, 3, 4, 0, 5, 2, 0, 4, 3, 100 };
+            Console.WriteLine ("Введите количество элементов массива");
+            int n = Int32.Parse (Console.ReadLine());
+            int[] mass = new int[n];
+            Console.WriteLine ("Введите элементы массива");
+            for (int i=0;i<n;i++)
+                mass[i] = Int32.Parse (Console.ReadLine ());
             int[] kol = new int[255];
             for (int i=0; i < mass.Length; i++)
                 kol[mass[i]]++;
             for (int i = 0; i<255 ; i++)
             {
                 if (kol[i] == 1)
-                    Console.WriteLine (i);
+                    Console.WriteLine ("Элемент который не повторяется "+i);
             }
             Console.ReadKey ();
         }
+
     }
 }
